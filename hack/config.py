a09ec1b6cd4a0911c7eff8433a1f8ad5fc682665
nvalidov <niyazvalidov@gmail.com>
2018-04-16 06:20:19 +0300

    accounts swap

--- hack/config.py
+++ hack/config.py
@@ -1,6 +1,6 @@
-ip = ''
+ip = '192.168.56.101'
 rpcPort = '8545'
-victimAccount = '0x8226b1072dbd69b3ee7d9a1ab05dce1f6dbe1861'
+victimAccount = '0xb81d5ec7a8ce67dc4601f72fc3d701ff71f2c513'
 hackerAccount = '0x652523c8aaf77ab898e4225a9dd31c419c6cd54f'
 amount = 1000000000000000000
 
