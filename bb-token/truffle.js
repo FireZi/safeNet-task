1ec90480cbd1509cff6dd20e5ab1258019662b76
nvalidov <niyazvalidov@gmail.com>
2018-04-16 03:16:08 +0300

    truffle config

--- bb-token/truffle.js
+++ bb-token/truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*",
+      from: "8226b1072dbd69b3ee7d9a1ab05dce1f6dbe1861",
+      gas: 6700000
     }
   }
 };
